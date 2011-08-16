f4d99bac983ee18e7ea8a9c4ed51bbaa689216b8
Martin Parm <mp@leasingborsen.dk>
2011-08-16 14:51:46 +0200

    Added documentation to SetOnSave-trait.

--- lib/KiokuDB/Meta/Attribute/SetOnSave.pm
+++ lib/KiokuDB/Meta/Attribute/SetOnSave.pm
@@ -12,3 +12,42 @@ has 'setonsave' => (
 );
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+KiokuDB::Meta::Attribute::SetOnSave - Trait for automatically updating attributes before collapsing
+
+=head1 SYNOPSIS
+
+    # in your class:
+
+    package Foo;
+    use Moose;
+    use Class::Date 'now';
+
+    has bar => (
+        traits    => [qw(KiokuDB::SetOnSave)],
+        isa       => "Class::Date",
+        is        => "ro",
+        setonsave => sub { now }
+    );
+
+=head1 DESCRIPTION
+
+This L<Moose::Meta::Attribute> trait tells L<KiokuDB> to update an attribute before collapsing.
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item setonsave
+
+A method which will be invoked just before the object is collapsed. setonsave is invoked with
+the object as it's first parameter and the attribute is updated with the return value, similarly
+to default and builder methods. 
+
+=cut
